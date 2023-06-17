17b88cc1c60b5dea486aab77cc3b398a8ef2db58
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-06-17 10:50:20 +0300

    feat: created reusable title label

---
+++ GithubbY/GHBodyLabel.swift
@@ -0,0 +1,20 @@
+//
+//  GHBodyLabel.swift
+//  GithubbY
+//
+//  Created by Дмитро Павлов on 17.06.2023.
+//
+
+import UIKit
+
+class GHBodyLabel: UILabel {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
