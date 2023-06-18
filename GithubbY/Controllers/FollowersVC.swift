1aad382a821403f0a74d8ea34aa9a3511ba559db
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-06-18 21:01:29 +0300

    feat: small refactor

--- GithubbY/Controllers/FollowersVC.swift
+++ GithubbY/Controllers/FollowersVC.swift
@@ -24,11 +24,26 @@ class FollowersVC: UIViewController {
     }
     
     private func configureCollectionView() {
-        followersCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UICollectionViewLayout())
+        followersCollectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
         view.addSubview(followersCollectionView)
         
         followersCollectionView.backgroundColor = .systemPink
         followersCollectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
+    }
+    
+    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout {
+        let width = view.bounds.width
+        let padding: CGFloat = 12
+        let minimumItemSpacing: CGFloat = 10
+        let availableWidth = width - (padding * 2) - (minimumItemSpacing * 2)
+        let itemWidth = availableWidth / 3
+        
+        let flowLayout = UICollectionViewFlowLayout()
+        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
+        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
+        
+        
+        return flowLayout
     }
     
     func getFollowers() {
