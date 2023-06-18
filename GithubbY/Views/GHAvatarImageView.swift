c8725158ad1ed8ce146a563f58a2fa301bea038b
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-06-18 20:05:32 +0300

    feat: create custom collection view cell

---
+++ GithubbY/Views/GHAvatarImageView.swift
@@ -0,0 +1,27 @@
+//
+//  GHAvatarImageView.swift
+//  GithubbY
+//
+//  Created by Дмитро Павлов on 18.06.2023.
+//
+
+import UIKit
+
+class GHAvatarImageView: UIImageView {
+    let placeholderImage = UIImage(named: "avatar-placeholder")!
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {
+        layer.cornerRadius = 10
+        clipsToBounds = true
+        image = placeholderImage
+        translatesAutoresizingMaskIntoConstraints = false
+    }
+}
