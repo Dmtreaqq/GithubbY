824868c58161c9a941db795fd6cc1880e90fffdb
Dmytro Pavlov <dmitrypavlov99@ukr.net>
2023-06-28 15:01:17 +0300

    feat: created header for userInfo

---
+++ GithubbY/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  GithubbY
+//
+//  Created by Дмитро Павлов on 28.06.2023.
+//
+
+import Foundation
+
+enum SFSymbols {
+    static let location = "mappin.and.ellipse"
+}
